ae9f683bd70b82764907cfd740c5e11e012afa03
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2016-10-18 07:09:03 +0100

    add tokenize and uniqueWords methods

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -3,4 +3,17 @@ class invertedIndex{
 		this.filesIndexed = {};
 		this.searchResults = {};
 	}
+    
+    tokenize(arr) {
+	    var tokenized = arr.replace(/[.,\/#!$%\^&@\*;:'{}=\-_`~()]/g, "").trim().toLowerCase().split(" ").sort();
+	    console.log(tokenized);
+	    return tokenized;    
+	}
+	
+	uniqueWords(arr) {
+		var tokens = this.tokenize(arr);
+		return tokens.filter(function (item, pos) {
+      		return tokens.indexOf(item) === pos;
+		});
+	}
 }
