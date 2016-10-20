e77a9966173f021a26542360830229212badd3a6
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2016-10-20 16:01:53 +0100

    refactor tokenize method and add comments

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,17 +1,36 @@
-class invertedIndex{
-	constructor(){
-		this.filesIndexed = {};
-		this.searchResults = {};
-	}
-    
-    tokenize(arr) {
-	    var tokenized = arr.replace(/[.,\/#!$%\^&@\*;:'{}=\-_`~()]/g, "").trim().toLowerCase().split(" ").sort();
-	    console.log(tokenized);
-	    return tokenized;    
+class InvertedIndex{
+	
+	constructor() {
+		/**
+		 * Initialize an empty array to hold the
+		 * concantenated words formed from title and text
+		 * contents.
+		 */ 
+		this.indexWords = [];
+		/**
+		 * Initialize an empty object to hold the mapping
+		 * of the words to their file location 
+		 */
+		this.index ={};
 	}
 	
-	uniqueWords(arr) {
-		var tokens = this.tokenize(arr);
+	/**
+	 * This Method returns an array of tokens/words from the provided input
+	 * after removing special characters and white spaces
+	 * @params{String} str - String to be tokonized
+	 */
+	tokenize(str) {
+	    var cleanString = str.trim().replace(/-/g, " ").replace(/[.,\/#!$%\^&@\*;:'{}=\_`~()]/g, "").toLowerCase().split(" ").sort();
+	    return cleanString;
+	}
+	
+	/**
+	 * This method returns an array of tokenized unique words
+	 * of the provided input
+	 * @param{String} str - The string to be filtered
+	 */
+	uniqueWords(str) {
+		var tokens = this.tokenize(str);
 		return tokens.filter(function (item, pos) {
       		return tokens.indexOf(item) === pos;
 		});
