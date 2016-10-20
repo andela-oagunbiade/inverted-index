2ca627bd5bab2d6f4b02111d72f6aebf39444ab0
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2016-10-20 18:33:39 +0100

    add Suite to check class creation,update Read Data Suite

--- spec/inverted-index-test.js
+++ spec/inverted-index-test.js
@@ -2,40 +2,34 @@
 
     //A test suite to read book data
 describe("Inverted Index Suite", function(){
-    let newIndex, book1, book2;
+    let newIndex, books;
         
     beforeEach(function(){
         //Create an instance of the Index class
-        newIndex = new Index();
+        newIndex = new InvertedIndex();
         
         //Test files to be used in the Index
-        book1 = [
-            {
-                "title": "Grouping Related Specs",
-                "text": "The describe function is for grouping related specs. The string parameter is for naming the collection of specs."
+        books = [
+            {"title": "Heroku",
+            "text":"You will be asked to enter your Heroku credentials the first time you run a command; after the first time, your email address and an API token will be saved"
             },
-            {
-                "title": "Setup and Teardown",
-                "text": "To help a test suite DRY up any duplicated setup and teardown code, Jasmine provides the global beforeEach and afterEach functions."
-            },
+            {"title": "Coveralls",
+            "text": "See the latest code-coverage statistics on all of your repositories including the total percentages covered and the lines covered."
+            }         
         ];
-        book2 = [
-            {
-                "title": "Hound Documentation",
-                "text": "Want to modify style rules to better suit your preferences? Below you will find documentation on configuring style rules for each linter that we support."
-            },
-            {
-                "title": "Hound Configuration",
-                "text": "All supported linters, except the ones in beta, are enabled by default. Hound will look for a custom configuration file named .hound.yml in the root directory of your project."
-            },
-        ];
+    });
+
+    describe("Class Inverted Index Class", function() {
+        it("Inverted Index should be a class", function(){
+            expect(typeof(newIndex)).toBe("object");
+        });
+
     });
 
     describe("Suite to Read Data", function(){
         it("JSON file should not be empty", function(){
-            const free=newIndex.createIndex(book1);
-            expect(free).toBeDefined();
-            expect(free[0].title).toBeDefined();
+            newIndex.createIndex(books);
+            expect(newIndex.index.heroku).toBeDefined();
         });
     });
         
