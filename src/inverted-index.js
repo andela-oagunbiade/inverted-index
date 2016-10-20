375c939c40f6db1f8574718040e14bf9ad7e3944
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2016-10-20 16:33:17 +0100

    add create index method

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -26,13 +26,53 @@ class InvertedIndex{
 	
 	/**
 	 * This method returns an array of tokenized unique words
-	 * of the provided input
-	 * @param{String} str - The string to be filtered
+	 * of the provided input 
+	 * @params{String} str - The string to be filtered
 	 */
 	uniqueWords(str) {
 		var tokens = this.tokenize(str);
 		return tokens.filter(function (item, pos) {
       		return tokens.indexOf(item) === pos;
 		});
+	}
+
+	/**
+	 * This method creates an index and updates the index object
+	 * Also populates the indexWords array with contents of supplied data
+	 * @params{Array} jsonData - The contents of the JSON file that is to be indexed
+	 */
+	createIndex(jsonData) {
+		for (let i of jsonData) {
+			//Iterate through the jsonData and store contents in an array
+			this.indexWords.push(i.title.toLowerCase() + " " + i.text.toLowerCase());
+		}
+		
+		/**
+		 * Join the contents of the array to create a string
+		 * And call the uniqueWords method on the string
+		 */
+		const uniqueContent = this.uniqueWords(this.indexWords.join(" "));
+		
+		/**
+		 * To maintain the context of the "this" keyword,
+		 * set it equal to a variable to be used in its place 
+		 */
+		var self = this;
+
+		/**
+		 * Create the index of the array by looping the uniqueContent
+		 * array and mapping the location of each word to its document
+		 * and adding the results into the Index object
+		 */
+		uniqueContent.forEach(function(word) {
+			self.index[word] = [];
+			
+			self.indexWords.forEach(function(document, pos) {
+				if (document.indexOf(word) > -1) {
+					self.index[word].push(pos); 
+				}
+			});
+		});
+		return self.index;
 	}
 }
