67ead27fa1561a23900f2ab4ccd0a9c3803a251a
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2016-10-23 23:23:16 +0100

    use angular to display search results in view

--- src/app.js
+++ src/app.js
@@ -4,23 +4,37 @@ let invertedApp = angular
     .module("InvertedIndex", [])
     .controller("invertedController", function($scope) {
         $scope.message = "Inverted Index";
+        let newIndex = new InvertedIndex();
         $scope.readFile = function(){
-            let newIndex = new InvertedIndex();
             let thefile = document.getElementById("selectFiles").files[0];
             let reader = new FileReader();
             reader.readAsText(thefile);
 
             reader.onload = function(e) {
-                let filed = JSON.parse(e.target.result);
-                let index = newIndex.createIndex(filed);
-                let range = [];
-                for(let i=0;i<filed.length;i++) {
-                    range.push(i);
+                if(!thefile.name.toLowerCase().match(/\.json$/)) {
+                    alert ("This is not a JSON file.");
+                    return;
                 }
-                $scope.filed =filed;
-                $scope.index = index;
-                $scope.range = range;
-                $scope.$apply();                
+                try{
+                    let filed = JSON.parse(e.target.result);
+                    let index = newIndex.createIndex(filed);
+                    let range = [];
+                    for(let i=0;i<filed.length;i++) {
+                        range.push(i);
+                    }
+                    $scope.filed =filed;
+                    $scope.index = index;
+                    $scope.range = range;
+                    $scope.$apply();
+                }catch(e) {
+                    alert(e);
+                }            
             };
+        };
+        $scope.searchFile = function() {
+            let searchItem = document.getElementById("searchFiles").value;
+            searchItem = searchItem.trim().toLowerCase();
+            let searchResults = newIndex.searchIndex(searchItem);
+            $scope.searchResults = searchResults;
         };
     });
