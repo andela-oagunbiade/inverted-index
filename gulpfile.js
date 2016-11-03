aad02855ae1eb9b9960fca44943ca2c5f8c06fd5
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2016-11-03 13:05:08 +0100

    update node version for use in travis

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const browserify = require('browserify');
@@ -7,7 +9,7 @@ const run = require('gulp-run');
 
 gulp.task('default', ['browser-sync', 'browserify', 'watch']);
 
-gulp.task('browser-sync', function () {
+gulp.task('browser-sync', () => {
   browserSync({
     server: {
       baseDir: './'
@@ -15,21 +17,21 @@ gulp.task('browser-sync', function () {
   });
 });
 
-gulp.task('reload', function () {
+gulp.task('reload',  () => {
   browserSync.reload();
 });
 
-gulp.task('watch', function () {
+gulp.task('watch',  () => {
   gulp.watch('./src/*.js', ['reload']);
   gulp.watch('./src/*.css', ['reload']);
   gulp.watch('./*.html', ['reload']);
 });
 
-gulp.task('test', ['browserify'], function () {
+gulp.task('test', ['browserify'],  () => {
   run('node_modules/karma/bin/karma start karma.conf.js --single-run').exec();
 });
 
-gulp.task('browserify', function () {
+gulp.task('browserify',  () => {
   return browserify('./spec/inverted-index-test.js')
     .bundle()
     .pipe(source('test-spec.js'))
